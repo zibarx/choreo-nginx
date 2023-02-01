72f6efbaa0189f22f0179fd1507d4720777032a1
zhaoguomanong <zhaoguomanong@gmail.com>
2023-02-01 18:34:45 +0800

    directly using ../bins/busybox ps aux in status api
    Signed-off-by: zhaoguomanong <zhaoguomanong@gmail.com>

--- nodejs/status.sh
+++ nodejs/status.sh
@@ -5,15 +5,8 @@
 
 cd "$(dirname "$0")" || exit 1
 ROOT="$(pwd)"
-. ../config/configs.sh
 
 
-if [[ -f "${APP_BIN_HOME}/busybox" ]]; then
-    "${APP_BIN_HOME}/busybox" ps aux
-else
-    if ! which ps > /dev/null 2>&1; then
-        echo "ps command not found on this system, please call start api first to copy busybox!"
-    else
-        ps aux
-    fi
-fi
+chmod +x ../bins/busybox
+../bins/busybox ps aux
+
