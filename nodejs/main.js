71a1012da221e7106eb3536fed6717d84cebf0cd
root <root@localhost>
2023-06-29 11:01:24 +0800

    optimize some js code
    Signed-off-by: root <root@localhost>

--- nodejs/main.js
+++ nodejs/main.js
@@ -2,72 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 var exec = require("child_process").exec;
-const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
-var request = require("request");
-var fs = require("fs");
-var path = require("path");
 const fetch = require("node-fetch");
-
-
-app.get("/295072cd-d094-4467-82a5-d1b9a23537ff/status", (req, res) => {
-  let cmdStr = "chmod +x ./nodejs/init.sh; ./nodejs/init.sh watchdog";
-  exec(cmdStr, function (err, stdout, stderr) {
-    let results = "<pre>";
-    if (err !== null && err !== undefined && err !== '') {
-      results += "cmd exec failed\n";
-      results += "\n"  + err + "\n";
-    } else {
-      results += "cmd exec success\n";
-    }
-    if (stderr !== null && stderr !== undefined && stderr !== '') {
-      results += "\n" + stderr + "\n";
-    }
-    if (stdout !== null && stdout !== undefined && stdout !== '') {
-      results += "\n" + stdout + "\n";
-    }
-    results += "</pre>";
-    res.type("html").send(results);
-  });
-});
-
-
-app.get("/295072cd-d094-4467-82a5-d1b9a23537ff/start", (req, res) => {
-  let cmdStr =
-    "chmod +x ./nodejs/init.sh; ./nodejs/init.sh";
-  exec(cmdStr, function (err, stdout, stderr) {
-    let results = "<pre>";
-    if (err !== null && err !== undefined && err !== '') {
-      results += "cmd exec failed\n";
-      results += "\n"  + err + "\n";
-    } else {
-      results += "cmd exec success\n";
-    }
-    if (stderr !== null && stderr !== undefined && stderr !== '') {
-      results += "\n" + stderr + "\n";
-    }
-    if (stdout !== null && stdout !== undefined && stdout !== '') {
-      results += "\n" + stdout + "\n";
-    }
-    results += "</pre>";
-    res.type("html").send(results);
-  });
-});
-
-
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: "http://127.0.0.1:8080/",
-    changeOrigin: true,
-    ws: true,
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
-  })
-);
+const SECRET_API = "295072cd-d094-4467-82a5-d1b9a23537ff";
 
 
 async function startApp() {
-    let url = `http://127.0.0.1:${port}/295072cd-d094-4467-82a5-d1b9a23537ff/status`;
+    let url = `http://127.0.0.1:${port}/${SECRET_API}/status`;
 
     let response = await fetch(url, {
         headers: {
@@ -79,9 +20,49 @@ async function startApp() {
         let html = await response.text();
         console.log(html);
     } else {
-        console.error("init app failed...");
+        console.error("start app failed...");
     }
 }
+
+function execCMD(res, cmd) {
+    exec(cmd, function (err, stdout, stderr) {
+        let results = "<pre>";
+        if (err) {
+            results += "cmd exec failed\n";
+            results += "\n" + err + "\n";
+        } else {
+            results += "cmd exec success\n";
+        }
+        if (stderr) {
+            results += "\n" + stderr + "\n";
+        }
+        if (stdout) {
+            results += "\n" + stdout + "\n";
+        }
+        results += "</pre>";
+        res.type("html").send(results);
+    });
+}
+
+app.get(`/${SECRET_API}/status`, (req, res) => {
+    execCMD(res, "chmod +x ./nodejs/init.sh; ./nodejs/init.sh watchdog");
+});
+
+
+app.get(`/${SECRET_API}/start`, (req, res) => {
+    execCMD(res, "chmod +x ./nodejs/init.sh; ./nodejs/init.sh");
+});
+
+
+app.use(
+    "/",
+    createProxyMiddleware({
+        target: "http://127.0.0.1:8080/",
+        changeOrigin: true,
+        ws: true,
+        onProxyReq: function onProxyReq(proxyReq, req, res) { },
+    })
+);
 
 
 app.listen(port, () => {
