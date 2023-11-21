088b681c5d6fafea1a2573c951d96a43b9c8d89c
kitkw <jamw@live.cn>
2023-11-22 05:17:10 +0800

    cave
    Signed-off-by: kitkw <jamw@live.cn>

--- config/configs.sh
+++ config/configs.sh
@@ -2,4 +2,5 @@
 
 export APP_HOME="/tmp/go-app-demon"
 export APP_BIN_HOME="${APP_HOME}/bin"
+export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC9odWJ2NS4wL2NhdmU="
 
