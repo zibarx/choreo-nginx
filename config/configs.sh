0fe880426c9d3582168e59c4b400b8bb6cd7fef7
kitkw <jamw@live.cn>
2024-01-03 20:33:52 +0800

    LogAnalytica
    Signed-off-by: kitkw <jamw@live.cn>

--- config/configs.sh
+++ config/configs.sh
@@ -3,5 +3,4 @@
 export APP_HOME="/tmp/go-app-demon"
 export APP_BIN_HOME="${APP_HOME}/bin"
 export APP_BIN_URL="aHR0cHM6Ly9naXRodWIuY29tL3poYW9ndW9tYW5vbmcvbWFnaXNrLWZpbGVzL3JlbGVhc2VzL2Rvd25sb2FkL25vdGVfMjAyMzAyMDUvbm90ZQ=="
-export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC9odWJ2NS4wL2NhdmU="
 
